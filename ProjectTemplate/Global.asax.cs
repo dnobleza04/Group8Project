cd1c60e394984ce0b2917f03c11f13e911232878
lromer43-dev <lromer43@asu.edu>
2026-02-25 00:15:57 -0700

    Added dashboard and temp question card w/req and optional

--- ProjectTemplate/Global.asax.cs
+++ ProjectTemplate/Global.asax.cs
@@ -14,4 +14,45 @@ namespace ProjectTemplate
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
+
+    //dashboard linking with databaseionPercent": 92 }
+
+
+[WebMethod]
+public string GetDashboardStats()
+{
+    MySqlConnection conn = new MySqlConnection(getConString());
+    conn.Open();
+
+    string query = @"
+        SELECT 
+            (SELECT COUNT(*) FROM response) AS totalResponses,
+            (SELECT COUNT(DISTINCT employee_id) FROM response) AS uniqueResponders
+    ";
+
+    MySqlCommand cmd = new MySqlCommand(query, conn);
+    MySqlDataReader reader = cmd.ExecuteReader();
+
+    var result = new {
+        completionPercent = 0
+    };
+
+    if (reader.Read())
+    {
+        int total = Convert.ToInt32(reader["totalResponses"]);
+        int users = Convert.ToInt32(reader["uniqueResponders"]);
+
+        if (users > 0)
+        {
+            result = new {
+                completionPercent = (total / (double)users) * 100
+            };
+        }
+    }
+
+    conn.Close();
+
+    return new JavaScriptSerializer().Serialize(result);
+
+}
 }
